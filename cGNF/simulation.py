101d8c2f29f4007ec669ec80d4328376d6399c18
Jesse Zhou <zhou.jesse2@gmail.com>
2025-07-02 23:43:32 -0500

    Update sim function

--- cGNF/simulation.py
+++ cGNF/simulation.py
@@ -17,7 +17,7 @@ def sim(path="", dataset_name="", model_name="models", n_mce_samples=10000, seed
     path_save = os.path.join(path, model_name)
 
     # Load the previously saved PyTorch model from the disk
-    model = torch.load(path_save + '/_best_model.pt', map_location=device)
+    model = torch.load(path_save + '/_best_model.pt', map_location=device, weights_only=False)
 
     # Load original dataset
     with open(path + dataset_name + '.pkl', 'rb') as f:
